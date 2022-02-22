be0339da4b124edd4f3cbf7e9676293cebb0dd6f
Akshata72 <sableakshata880@gmail.com>
2022-02-22 23:15:30 +0530

    Added:UC2 logic

--- flip_coin.sh
+++ flip_coin.sh
@@ -1,14 +1,21 @@
 
 head=1
 tail=0
-
+head_count=0
+tail_count=0
+for((i=1; i<=100; i++))
+do
 result=$((RANDOM%2))
 
 if (($result==1))
 then
-echo "winner"=$head
-else  echo "losser"=$tail
+     ((head_count++))
+else
+     ((tail_count++))
 fi
+done
 
-
-
+echo "winner"=$head
+echo "losser"=$tail
+echo "No.of times head won"=$head_count
+echo "No. of time tail won"=$tail_count
